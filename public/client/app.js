7ac7f36156086bf585a7da3893889b2ebd6a96e7
Juan Marin <juan@codecery.com>
2014-02-07 16:07:34 -0800

    Loads templates and posts to server

--- public/client/app.js
+++ public/client/app.js
@@ -1,52 +1,41 @@
-window.Shortly = Backbone.View.extend({
+var shortlyApp = angular.module('shortlyApp', [
+  'ngRoute'
+]);
 
-  template: _.template(' \
-      <h1>Shortly</h1> \
-      <div class="navigation"> \
-      <ul> \
-        <li><a href="#" class="index">All Links</a></li> \
-        <li><a href="#" class="create">Shorten</a></li> \
-      </ul> \
-      </div> \
-      <div id="container"></div>'
-  ),
+shortlyApp.config(['$routeProvider',
+  function($routeProvider) {
+  $routeProvider.
+    when('/', {
+      templateUrl: 'client/partials/listUrls.html',
+      controller: 'UrlListCtrl'
+    }).
+    when('/create', {
+      templateUrl: 'client/partials/createUrl.html',
+      controller: 'UrlCreateCtrl'
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
+}]);
 
-  events: {
-    "click li a.index":  "renderIndexView",
-    "click li a.create": "renderCreateView"
-  },
-
-  initialize: function(){
-    console.log( "Shortly is running" );
-    $('body').append(this.render().el);
-    this.renderIndexView(); // default view
-  },
-
-  render: function(){
-    this.$el.html( this.template() );
-    return this;
-  },
-
-  renderIndexView: function(e){
-    e && e.preventDefault();
-    var links = new Shortly.Links();
-    var linksView = new Shortly.LinksView( {collection: links} );
-    this.$el.find('#container').html( linksView.render().el );
-    this.updateNav('index');
-  },
-
-  renderCreateView: function(e){
-    e && e.preventDefault();
-    var linkCreateView = new Shortly.LinkCreateView();
-    this.$el.find('#container').html( linkCreateView.render().el );
-    this.updateNav('create');
-  },
-
-  updateNav: function(className){
-    this.$el.find('.navigation li a')
-            .removeClass('selected')
-            .filter('.'+className)
-            .addClass('selected');
-  }
-
+shortlyApp.controller('UrlListCtrl', function($scope, $http) {
+  $http({
+    method: 'GET',
+    url: '/links'
+  }).then(function(obj){
+    $scope.links = obj.data;
+  });
 });
+
+shortlyApp.controller('UrlCreateCtrl', function($scope, $http) {
+  $scope.shorten = function(url) {
+    $http({
+      method: 'POST',
+      url: '/links',
+      data: {url: url}
+    }).then(function(data){
+      console.log(data);
+    });
+  };
+});
+
